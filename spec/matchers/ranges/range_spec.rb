9475b548cdc4279b14c7b5a750eb668226a92866
Jaqueline Camarda <jaquelineassisoliveira@gmail.com>
2019-11-19 19:36:55 -0200

    Continuação Matchers

--- spec/matchers/ranges/range_spec.rb
+++ spec/matchers/ranges/range_spec.rb
@@ -1,6 +1,10 @@
 describe (1..5), 'Ranges' do
     it '#cover' do
+        is_expected.to cover(2)
         expect(subject).to cover(2)
         expect(subject).to cover(2,5)
+        expect(subject).not_to cover(0,6)
     end
+    #em uma unica linha mais fácil de entender 
+    it { is_expected.to cover(2) }
 end
