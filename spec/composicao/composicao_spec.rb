e582685f2fc92db25cabcfe5b0469cca7d330a63
Jaqueline Camarda <jaquelineassisoliveira@gmail.com>
2019-11-20 18:44:32 -0200

    Helper methods

--- spec/composicao/composicao_spec.rb
+++ spec/composicao/composicao_spec.rb
@@ -1,10 +1,15 @@
+require_relative 'helper'
+
+RSpec.configure do |conf|
+    conf.include Helper
+end
+
 describe 'Ruby on Rails' do
     it {is_expected.to start_with("Ruby").and end_with("Rails")}
     it {expect(fruta).to eq('banana').or eq('laranja').or eq('uva')}
 
-    def fruta
-        %w(banana laranja uva).sample #.sample é sorteio aleatório
-
-    end
+    #def fruta
+       # %w(banana laranja uva).sample #.sample é sorteio aleatório
+    #end
 
 end
---
+++ spec/composicao/helper.rb
@@ -0,0 +1,5 @@
+module Helper 
+  def fruta
+    %w(banana laranja uva).sample #.sample é sorteio aleatório
+  end
+end
