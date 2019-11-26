26f5fc6a7e1fc28a075354b17ac444e0d364edd1
Jaqueline Camarda <jaquelineassisoliveira@gmail.com>
2019-11-26 20:51:33 -0200

    Matchers

---
+++ spec/matchers/atributos/atributos_let_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'pessoa'
+
+describe 'atributos' do
+
+  let(:pessoa) { Pessoa.new }
+  
+  it 'have_attributes' do
+    pessoa = Pessoa.new
+    pessoa.nome = "Jaque"
+    pessoa.idade = 26
+
+    expect(pessoa).to have_attributes(nome: starting_with("J"), idade: (be >= 26))
+  end
+end
